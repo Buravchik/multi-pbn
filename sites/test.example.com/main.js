07450cc3795d02adee1d31f9315197cd8bb5fadb
Alexey Isachenko <bezmerno@gmail.com>
2025-09-18 11:28:25 +0200

    cleanup

--- sites/test.example.com/main.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const el = document.createElement('p');
-  el.textContent = `Deployed at ${new Date().toLocaleString()}`;
-  document.querySelector('main')?.appendChild(el);
-});
-
